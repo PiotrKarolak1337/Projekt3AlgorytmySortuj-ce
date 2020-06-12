7ecc4b21fca6782f2f3e681718a40ddc875ef443
PiotrKarolak1337 <63090660+PiotrKarolak1337@users.noreply.github.com>
2020-06-13 00:16:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,441 @@
+﻿using System;
+using System.Numerics;
+using System.Diagnostics;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static int[] TabLos(int rozmiar)
+        {
+            int[] tab = new int[rozmiar];
+
+            for (int i = 0; i < rozmiar; i++)
+            {
+                tab[i] = Losuj();               
+
+            }           
+            return (tab); // zwraca tablicę
+        }
+        static int Losuj()
+        {
+            Random r = new Random();
+            return r.Next(0, 100);
+        }
+
+
+        public static int[] TabVShape(int rozmiar)
+        {
+
+            int[] tab = new int[rozmiar];
+
+            int count = 0;
+
+            for (int i = rozmiar / 2; i > 0; i--)
+            {
+                tab[count++] = i;
+            }
+
+            for (int i = 0; i < rozmiar / 2; i++)
+            {
+                tab[count++] = i;
+            }
+            return (tab);
+        }
+        private static int[] TabRos(int rozmiar)
+        {
+            int count = 0;
+            int[] tab = new int[rozmiar];
+
+            for (int i = 0; i < rozmiar; i++)
+            {
+                tab[count++] = i;
+            }
+
+            return (tab);
+        }
+        private static int[] TabMal(int rozmiar)
+        {
+            int count = 0;
+            int[] tab = new int[rozmiar];
+            for (int i = rozmiar; i > 0; i--)
+            {
+                tab[count++] = i;
+            }
+            return (tab);
+
+        }
+        private static int[] TabStala(int rozmiar)
+        {
+            int count = 0;
+            int[] tab = new int[rozmiar];
+
+            for (int i = 0; i < rozmiar; i++)
+            {
+                tab[count++] = 0;
+            }
+
+            return (tab);
+        }
+        static int[] InsertionSort(int[] inputArray)
+        {
+            for (int i = 0; i < inputArray.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (inputArray[j - 1] > inputArray[j])
+                    {
+                        int temp = inputArray[j - 1];
+                        inputArray[j - 1] = inputArray[j];
+                        inputArray[j] = temp;
+
+                    }
+
+                }
+            }
+
+            return inputArray;
+        }
+        static int[] SelectionSort(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int min_idx = i;
+                for (int j = i + 1; j < n; j++)
+                    if (arr[j] < arr[min_idx])
+                        min_idx = j;
+                int temp = arr[min_idx];
+                arr[min_idx] = arr[i];
+                arr[i] = temp;
+            }
+            return (arr);
+        }
+
+        static int[] CocktailSort(int[] a)
+        {
+            bool swapped = true;
+            int start = 0;
+            int end = a.Length;
+
+            while (swapped == true)
+            {
+
+                swapped = false;
+
+                for (int i = start; i < end - 1; ++i)
+                {
+                    if (a[i] > a[i + 1])
+                    {
+                        int temp = a[i];
+                        a[i] = a[i + 1];
+                        a[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                if (swapped == false)
+                    break;
+
+                swapped = false;
+
+                end = end - 1;
+
+                for (int i = end - 1; i >= start; i--)
+                {
+                    if (a[i] > a[i + 1])
+                    {
+                        int temp = a[i];
+                        a[i] = a[i + 1];
+                        a[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+                start = start + 1;
+            }
+            return (a);
+        }
+
+        private static int[] HeapSort(int[] tab)
+        {
+            int left = tab.Length / 2;
+            int right = tab.Length - 1;
+            while (left > 0)
+            {
+                left--;
+                Heapify(ref tab, left, right);
+            }
+
+            while (right > 0)
+            {
+                int temp = tab[left];
+                tab[left] = tab[right];
+                tab[right] = temp;
+                right--;
+                Heapify(ref tab, left, right);
+            }
+            return tab;
+        }
+
+        private static int[] Heapify(ref int[] tab, int left, int right)
+        {
+            int i = left,
+                j = 2 * i + 1;
+            int temp = tab[i];
+            while (j <= right)
+            {
+                if (j < right)
+                    if (tab[j] < tab[j + 1])
+                        j++;
+                if (temp >= tab[j]) break;
+                tab[i] = tab[j];
+                i = j;
+                j = 2 * i + 1;
+            }
+
+            tab[i] = temp;
+            return tab;
+        }
+        static void AddToFile(string type, BigInteger zapis)
+        {
+            string path = @"dane.txt";
+
+            if (!File.Exists(path))
+            {
+                //Tworzenie pliku
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine($"{type}, {zapis}");
+                }
+            }
+
+            //Wypełnianie pliku danymi
+            using (StreamWriter sw = File.AppendText(path))
+            {
+
+                sw.WriteLine($"{type},{zapis}");
+            }
+        }
+        static void Main(string[] args)
+        {
+
+
+            
+            for (int j = 50_000; j < 200_000; j +=5_000)
+                {                                    
+                    Stopwatch stoper = new Stopwatch();
+                    stoper.Reset();
+                    stoper.Start();
+                    int[] tab11 = InsertionSort(TabLos(j));//Tablica nr 1 po InsertionSort
+                    stoper.Stop();
+                    BigInteger czasWykonania = stoper.ElapsedTicks;
+                    AddToFile("Zliczanie czasu InsertionSort Losowa", czasWykonania);
+                    Console.WriteLine(j);
+                }
+               
+            for (int j = 50_000; j < 200_000; j +=5000)
+                {
+                    Stopwatch stoper = new Stopwatch();
+                    stoper.Reset();
+                    stoper.Start();
+                    int[] tab22 = InsertionSort(TabVShape(j));//Tablica nr 2 po InsertionSort 
+                    BigInteger czasWykonania = stoper.ElapsedTicks;
+                    AddToFile("Zliczanie czasu InsertionSort VShape", czasWykonania);
+                    Console.WriteLine(j);
+                }
+                            
+            for (int j = 50_000; j < 200_000; j += 5_000)
+                {
+                    
+                    Stopwatch stoper = new Stopwatch();
+                    stoper.Reset();
+                    stoper.Start();
+                    int[] tab33 = InsertionSort(TabRos(j));//Tablica nr 3 po InsertionSort
+                    BigInteger czasWykonania = stoper.ElapsedTicks;
+                    AddToFile("Zliczanie czasu InsertionSort TabRos", czasWykonania);
+                    Console.WriteLine(j);
+            }
+
+            for (int j = 50_000; j < 200_000; j += 5_000)
+                {
+                    Stopwatch stoper = new Stopwatch();
+                    stoper.Reset();
+                    stoper.Start();
+                    int[] tab44 = InsertionSort(TabMal(j));//Tablica nr 4 po InsertionSort
+                    BigInteger czasWykonania = stoper.ElapsedTicks;
+                    AddToFile("Zliczanie czasu InsertionSort TabMal", czasWykonania);
+                    Console.WriteLine(j);
+            }
+            for (int j = 50_000; j < 200_000; j += 5_000)
+                {
+                    Stopwatch stoper = new Stopwatch();
+                    stoper.Reset();
+                    stoper.Start();
+                    int[] tab55 = InsertionSort(TabStala(j));//Tablica nr 5 po InsertionSort
+                    BigInteger czasWykonania = stoper.ElapsedTicks;
+                    AddToFile("Zliczanie czasu InsertionSort TabStala", czasWykonania);
+                    Console.WriteLine(j);
+            }
+            Console.WriteLine("Koniec InsertionSort");
+            for (int j = 50_000; j < 200_000; j += 5_000)
+            {
+                Stopwatch stoper = new Stopwatch();
+                stoper.Reset();
+                stoper.Start();
+                int[] tab111 = SelectionSort(TabLos(j));//Tablica nr 1 po SelectionSort
+                BigInteger czasWykonania = stoper.ElapsedTicks;
+                AddToFile("Zliczanie czasu SelectionSort TabLos", czasWykonania);
+                Console.WriteLine(j);
+            }
+            for (int j = 50_000; j < 200_000; j += 5_000)
+            {
+                Stopwatch stoper = new Stopwatch();
+                stoper.Reset();
+                stoper.Start();
+                int[] tab111 = SelectionSort(TabVShape(j));//Tablica nr 2 po SelectionSort
+                BigInteger czasWykonania = stoper.ElapsedTicks;
+                AddToFile("Zliczanie czasu SelectionSort TabVShape", czasWykonania);
+                Console.WriteLine(j);
+            }
+            for (int j = 50_000; j < 200_000; j += 5_000)
+            {
+                Stopwatch stoper = new Stopwatch();
+                stoper.Reset();
+                stoper.Start();
+                int[] tab111 = SelectionSort(TabRos(j));//Tablica nr 3 po SelectionSort
+                BigInteger czasWykonania = stoper.ElapsedTicks;
+                AddToFile("Zliczanie czasu SelectionSort TabRos", czasWykonania);
+                Console.WriteLine(j);
+            }
+            for (int j = 50_000; j < 200_000; j += 5_000)
+            {
+                Stopwatch stoper = new Stopwatch();
+                stoper.Reset();
+                stoper.Start();
+                int[] tab111 = SelectionSort(TabMal(j));//Tablica nr 4 po SelectionSort
+                BigInteger czasWykonania = stoper.ElapsedTicks;
+                AddToFile("Zliczanie czasu SelectionSort TabMal", czasWykonania);
+                Console.WriteLine(j);
+            }
+            for (int j = 50_000; j < 200_000; j += 5_000)
+            {
+                Stopwatch stoper = new Stopwatch();
+                stoper.Reset();
+                stoper.Start();
+                int[] tab111 = SelectionSort(TabStala(j));//Tablica nr 5 po SelectionSort
+                BigInteger czasWykonania = stoper.ElapsedTicks;
+                AddToFile("Zliczanie czasu SelectionSort TabStala", czasWykonania);
+                Console.WriteLine(j);
+            }
+            Console.WriteLine("Koniec SelectionSort");
+            for (int j = 50_000; j < 200_000; j += 5_000)
+            {
+                Stopwatch stoper = new Stopwatch();
+                stoper.Reset();
+                stoper.Start();
+                int[] tab111 = HeapSort(TabLos(j));//Tablica nr 1 po HeapSort
+                BigInteger czasWykonania = stoper.ElapsedTicks;
+                AddToFile("Zliczanie czasu HeapSort TabLos", czasWykonania);
+                Console.WriteLine(j);
+            }
+            for (int j = 50_000; j < 200_000; j += 5_000)
+            {
+                Stopwatch stoper = new Stopwatch();
+                stoper.Reset();
+                stoper.Start();
+                int[] tab111 = HeapSort(TabVShape(j));//Tablica nr 2 po HeapSort
+                BigInteger czasWykonania = stoper.ElapsedTicks;
+                AddToFile("Zliczanie czasu HeapSort TabVShape", czasWykonania);
+                Console.WriteLine(j);
+            }
+            for (int j = 50_000; j < 200_000; j += 5_000)
+            {
+                Stopwatch stoper = new Stopwatch();
+                stoper.Reset();
+                stoper.Start();
+                int[] tab111 = HeapSort(TabRos(j));//Tablica nr 3 po HeapSort
+                BigInteger czasWykonania = stoper.ElapsedTicks;
+                AddToFile("Zliczanie czasu HeapSort TabRos", czasWykonania);
+                Console.WriteLine(j);
+            }
+            for (int j = 50_000; j < 200_000; j += 5_000)
+            {
+                Stopwatch stoper = new Stopwatch();
+                stoper.Reset();
+                stoper.Start();
+                int[] tab111 = HeapSort(TabMal(j));//Tablica nr 4 po HeapSort
+                BigInteger czasWykonania = stoper.ElapsedTicks;
+                AddToFile("Zliczanie czasu HeapSort TabMal", czasWykonania);
+                Console.WriteLine(j);
+            }
+            for (int j = 50_000; j < 200_000; j += 5_000)
+            {
+                Stopwatch stoper = new Stopwatch();
+                stoper.Reset();
+                stoper.Start();
+                int[] tab111 = HeapSort(TabStala(j));//Tablica nr 5 po HeapSort
+                BigInteger czasWykonania = stoper.ElapsedTicks;
+                AddToFile("Zliczanie czasu HeapSort TabStala", czasWykonania);
+                Console.WriteLine(j);
+            }
+            Console.WriteLine("Koniec HeapSort");
+            for (int j = 50_000; j < 200_000; j += 5_000)
+            {
+                Stopwatch stoper = new Stopwatch();
+                stoper.Reset();
+                stoper.Start();
+                int[] tab111 = CocktailSort(TabLos(j));//Tablica nr 1 po Cocktail
+                BigInteger czasWykonania = stoper.ElapsedTicks;
+                AddToFile("Zliczanie czasu Cocktail TabLos", czasWykonania);
+                Console.WriteLine(j);
+            }
+            for (int j = 50_000; j < 200_000; j += 5_000)
+            {
+                Stopwatch stoper = new Stopwatch();
+                stoper.Reset();
+                stoper.Start();
+                int[] tab111 = CocktailSort(TabVShape(j));//Tablica nr 2 po Cocktail
+                BigInteger czasWykonania = stoper.ElapsedTicks;
+                AddToFile("Zliczanie czasu Cocktail TabVShape", czasWykonania);
+                Console.WriteLine(j);
+            }
+            for (int j = 50_000; j < 200_000; j += 5_000)
+            {
+                Stopwatch stoper = new Stopwatch();
+                stoper.Reset();
+                stoper.Start();
+                int[] tab111 = CocktailSort(TabRos(j));//Tablica nr 3 po Cocktail
+                BigInteger czasWykonania = stoper.ElapsedTicks;
+                AddToFile("Zliczanie czasu Cocktail TabRos", czasWykonania);
+                Console.WriteLine(j);
+            }
+            for (int j = 50_000; j < 200_000; j += 5_000)
+            {
+                Stopwatch stoper = new Stopwatch();
+                stoper.Reset();
+                stoper.Start();
+                int[] tab111 = CocktailSort(TabMal(j));//Tablica nr 4 po Cocktail
+                BigInteger czasWykonania = stoper.ElapsedTicks;
+                AddToFile("Zliczanie czasu Cocktail TabMal", czasWykonania);
+                Console.WriteLine(j);
+            }
+            for (int j = 50_000; j < 200_000; j += 5_000)
+            {
+                Stopwatch stoper = new Stopwatch();
+                stoper.Reset();
+                stoper.Start();
+                int[] tab111 = CocktailSort(TabStala(j));//Tablica nr 5 po Cocktail
+                BigInteger czasWykonania = stoper.ElapsedTicks;
+                AddToFile("Zliczanie czasu Cocktail TabStala", czasWykonania);
+                Console.WriteLine(j);
+            }
+            Console.WriteLine("Koniec Cocktail");
+            Console.WriteLine("Koniec");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ QuickSort.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading;
+
+namespace Quick_Sort
+{
+    class Program
+    {
+        static void QsortItera(int[] t)
+        {
+            int i, j, l, p, sp;
+            int[] stos_l = new int[t.Length],
+            stos_p = new int[t.Length]; 
+            sp = 0; stos_l[sp] = 0; stos_p[sp] = t.Length - 1; 
+            do
+            {
+                l = stos_l[sp]; p = stos_p[sp]; sp--; 
+                do
+                {
+                    int x;
+                    i = l; j = p; x = t[(l + p) / 2]; 
+                    do
+                    {
+                        while (t[i] < x) i++;
+                        while (x < t[j]) j--;
+                        if (i <= j)
+                        {
+                            int buf = t[i]; t[i] = t[j]; t[j] = buf;
+                            i++; j--;
+                        }
+                    } while (i <= j);
+                    if (i < p) { sp++; stos_l[sp] = i; stos_p[sp] = p; } 
+                    p = j;
+                } while (l < p);
+            } while (sp >= 0); 
+        }
+        static int[] TabLos(int rozmiar)
+        {
+            int[] tab = new int[rozmiar];
+
+            for (int i = 0; i < rozmiar; i++)
+            {
+                tab[i] = Losuj();
+                
+
+            }
+            
+            return (tab); 
+        }
+        static int Losuj()
+        {
+            Random r = new Random();
+            return r.Next(0, 100);
+        }
+
+        static void AddToFile(string type, BigInteger zapis)
+        {
+            string path = @"dane.txt";
+
+            if (!File.Exists(path))
+            {
+                
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine($"{type}, {zapis}");
+                }
+            }
+
+            
+            using (StreamWriter sw = File.AppendText(path))
+            {
+
+                sw.WriteLine($"{type},{zapis}");
+            }
+        }
+        static void QsortReku(int[] t, int l, int p)
+        {
+            int i, j, x;
+            i = l;
+            j = p;
+            x = t[(l + p) / 2]; 
+            do
+            {
+                while (t[i] < x) i++; 
+                while (x < t[j]) j--; 
+                if (i <= j) 
+                { 
+                    int buf = t[i]; t[i] = t[j]; t[j] = buf;
+                    i++; j--;
+                }
+            }
+            while (i <= j);
+            if (l < j) QsortReku(t, l, j); 
+            if (i < p) QsortReku(t, i, p); 
+        }
+        static int[] TabAShape(int size)
+        {
+            int constant = size / 2;
+            int[] table = new int[size];
+            for (int i = 0; i < table.Length; i++)
+            {
+                if (i < table.Length / 2)
+                {
+                    table[i] = i;
+                }
+                else
+                {
+                    table[i] = table.Length - 1 - i;
+
+                }
+                
+            }
+            return table;
+        }
+        static void QuickSortIterativeMid(int[] tab)
+        {
+            int i, j, l, r, sp;
+            int[] stackLeft = new int[tab.Length],
+                stackRight = new int[tab.Length];
+            sp = 0;
+            stackLeft[sp] = 0;
+            stackRight[sp] = tab.Length - 1;
+
+            do
+            {
+                l = stackLeft[sp];
+                r = stackRight[sp];
+                sp--;
+
+                do
+                {
+                    int x;
+                    i = l;
+                    j = r;
+                    x = tab[(l + r) / 2];
+
+                    do
+                    {
+                        while (tab[i] < x) i++;
+                        while (x < tab[j]) j--;
+                        if (i <= j)
+                        {
+                            int buf = tab[i];
+                            tab[i] = tab[j];
+                            tab[j] = buf;
+                            i++;
+                            j--;
+                        }
+                    } while (i <= j);
+
+                    if (i < r)
+                    {
+                        sp++;
+                        stackLeft[sp] = i;
+                        stackRight[sp] = r;
+                    }
+
+                    r = j;
+                } while (l < r);
+            } while (sp >= 0);
+        }
+        static void QuickSortIterativeRight(int[] tab)
+        {
+            int i, j, l, r, sp;
+            int[] stackLeft = new int[tab.Length],
+                stackRight = new int[tab.Length];
+            sp = 0;
+            stackLeft[sp] = 0;
+            stackRight[sp] = tab.Length - 1;
+
+            do
+            {
+                l = stackLeft[sp];
+                r = stackRight[sp];
+                sp--;
+
+                do
+                {
+                    int x;
+                    i = l;
+                    j = r;
+
+                    x = tab[(r)];
+
+                    do
+                    {
+                        while (tab[i] < x) i++;
+                        while (x < tab[j]) j--;
+                        if (i <= j)
+                        {
+                            int buf = tab[i];
+                            tab[i] = tab[j];
+                            tab[j] = buf;
+                            i++;
+                            j--;
+                        }
+                    } while (i <= j);
+
+                    if (i < r)
+                    {
+                        sp++;
+                        stackLeft[sp] = i;
+                        stackRight[sp] = r;
+                    }
+
+                    r = j;
+                } while (l < r);
+            } while (sp >= 0);
+        }
+        static void QuickSortIterativeRandom(int[] tab)
+        {
+            
+            Random random = new Random();
+            int i, j, l, r, sp;
+            int[] stackLeft = new int[tab.Length],
+                stackRight = new int[tab.Length];
+            sp = 0;
+            stackLeft[sp] = 0;
+            stackRight[sp] = tab.Length - 1;
+
+            do
+            {
+                l = stackLeft[sp];
+                r = stackRight[sp];
+                sp--;
+
+                do
+                {
+                    int x;
+                    i = l;
+                    j = r;
+                    
+                    x = tab[random.Next(0, tab.Length -1)];
+
+                    do
+                    {
+                        while (tab[i] < x) i++;
+                        while (x < tab[j]) j--;
+                        if (i <= j)
+                        {
+                            int buf = tab[i];
+                            tab[i] = tab[j];
+                            tab[j] = buf;
+                            i++;
+                            j--;
+                            
+                        }
+                    } while (i <= j);
+
+                    if (i < r)
+                    {
+                        sp++;
+                        stackLeft[sp] = i;
+                        stackRight[sp] = r;
+                    }
+
+                    r = j;
+                } while (l < r);
+            } while (sp >= 0);
+            Console.WriteLine("coś");
+        }
+        static void Tester()
+        {
+            for (int j = 130_000; j < 200_000; j += 5_000)
+            {
+                Stopwatch stoper = new Stopwatch();
+                stoper.Reset();
+                stoper.Start();
+                int[] arr = TabAShape(j);
+                QsortReku(arr, 0, arr.Length - 1);
+                stoper.Stop();
+                BigInteger czasWykonania = stoper.ElapsedTicks;
+                AddToFile("Zliczanie czasu rekurencyjna AShape", czasWykonania);
+                Console.WriteLine(j);
+            }
+            
+
+            for (int j = 50_000; j < 200_000; j += 5_000)
+            {
+                Stopwatch stoper = new Stopwatch();
+                stoper.Reset();
+                stoper.Start();
+                int[] arr = TabAShape(j);
+                QuickSortIterativeMid(arr);
+                stoper.Stop();
+                BigInteger czasWykonania = stoper.ElapsedTicks;
+                AddToFile("Zliczanie czasu rekurencyjna środkowa", czasWykonania);
+                Console.WriteLine(j);
+            }
+            for(int j = 50_000; j < 200_000; j += 5_000)
+            {
+                Stopwatch stoper = new Stopwatch();
+                stoper.Reset();
+                stoper.Start();
+                int[] arr = TabAShape(j);
+                QuickSortIterativeRight(arr);
+                stoper.Stop();
+                BigInteger czasWykonania = stoper.ElapsedTicks;
+                AddToFile("Zliczanie czasu rekurencyjna prawa", czasWykonania);
+                Console.WriteLine(j);
+            }
+            
+            for (int j = 50_000; j < 200_000; j += 5_000)
+            {
+                Stopwatch stoper = new Stopwatch();
+                stoper.Reset();
+                stoper.Start();
+                int[] arr = TabAShape(j);
+                QuickSortIterativeRandom(arr);
+                stoper.Stop();
+                BigInteger czasWykonania = stoper.ElapsedTicks;
+                AddToFile("Zliczanie czasu rekurencyjna randomowa", czasWykonania);
+                Console.WriteLine(j);
+            }
+        }
+        static void Main(string[] args)
+        {
+            for (int j = 50_000; j < 200_000; j += 5_000)
+            {
+                Stopwatch stoper = new Stopwatch();
+                stoper.Reset();
+                stoper.Start();
+                int[] arr = TabLos(j);
+                QsortItera(arr);
+                stoper.Stop();
+                BigInteger czasWykonania = stoper.ElapsedTicks;
+                AddToFile("Zliczanie czasu iteracyjna losowa tab", czasWykonania);
+                Console.WriteLine(j);
+            }
+            for (int j = 50_000; j < 200_000; j += 5_000)
+            {
+                Stopwatch stoper = new Stopwatch();
+                stoper.Reset();
+                stoper.Start();
+                int[] arr = TabLos(j);
+                QsortReku(arr,0,arr.Length -1);
+                stoper.Stop();
+                BigInteger czasWykonania = stoper.ElapsedTicks;
+                AddToFile("Zliczanie czasu rekurencyjna VShape", czasWykonania);
+                Console.WriteLine(j);
+            }
+            for (int j = 50_000; j < 200_000; j += 5_000)
+            {
+                Stopwatch stoper = new Stopwatch();
+                stoper.Reset();
+                stoper.Start();
+                int[] arr = TabAShape(j);
+                QsortItera(arr);
+                stoper.Stop();
+                BigInteger czasWykonania = stoper.ElapsedTicks;
+                AddToFile("Zliczanie czasu iteracyjna AShape", czasWykonania);
+                Console.WriteLine(j);
+            }
+            
+            Thread TesterThread = new Thread(Program.Tester, 8 * 1024 * 1024); 
+            TesterThread.Start(); 
+            TesterThread.Join(); 
+
+
+
+
+
+            Console.WriteLine();
+        }
+    }
+}
